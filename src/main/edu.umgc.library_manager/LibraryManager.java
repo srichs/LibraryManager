86e3f423a6e1cb7afb94117a90bbf2646516d6d7
srichs <sr@srich.us>
2020-10-28 02:14:53 -0400

    Created the main class of the application

---
+++ src/main/edu.umgc.library_manager/LibraryManager.java
@@ -0,0 +1,18 @@
+/*
+ * Filename: LibraryManager.java
+ * Author: srichs
+ * Date Created: 10/28/2020
+ * Purpose: This class contains the main method of the LibraryManager application.
+ */
+
+
+/**
+ * The main class of the LibraryManager application
+ */
+public class LibraryManager {
+
+    public static void main(String[] args) {
+        System.out.println("LibraryManager");
+    }
+
+}
