9328b3fe52bd3dac5f6d84cd632b7d6eca3c1fdc
srichs <sr@srich.us>
2020-11-21 18:17:03 -0500

    Updated files

--- src/main/java/edu/umgc/librarymanager/data/UserException.java
+++ src/main/java/edu/umgc/librarymanager/data/UserException.java
@@ -6,6 +6,10 @@
 
 package edu.umgc.librarymanager.data;
 
+/**
+ * An Exception to be thrown for an error involving a User.
+ * @author Scott
+ */
 public class UserException extends Exception {
 
     private static final long serialVersionUID = 1L;
--- src/main/java/edu/umgc/librarymanager/data/UserLoginDAO.java
+++ src/main/java/edu/umgc/librarymanager/data/UserLoginDAO.java
@@ -1,84 +1,48 @@
 /*
  * Filename: UserLoginDAO.java
- * Author: srichs
- * Date Created: 11/10/2020
- * Purpose: This class is used to create a DAO for the User Login information.
+ * Author: Scott
+ * Date Created: 11/21/2020
  */
 
 package edu.umgc.librarymanager.data;
 
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 /**
  * Used to create a data access object for the user login information.
+ * @author Scott
  */
-public class UserLoginDAO implements DAOInteface<UserLogin> {
+public class UserLoginDAO extends BaseDAO implements DAOInteface<UserLogin> {
 
-    private Session session;
-    private Transaction transaction;
+    public UserLoginDAO() {}
 
-    public UserLoginDAO() {
-    }
-
-    public Session openSession() {
-        session = HibernateUtility.getSessionFactory().openSession();
-        return session;
+    /**
+     * Searches for whether a user exists in the database.
+     * @param username The user's username.
+     * @return A boolean value for whether the user exists.
+     */
+    @SuppressWarnings("unchecked")
+    public boolean searchByUsername(String username) {
+        List<UserLogin> logins = (List<UserLogin>) getSession()
+                .createQuery("Select login From user_login Where login.username = :uname")
+                .setParameter("uname", username).getResultList();
+        if (logins.size() > 0) {
+            if (logins.get(0).getUsername() == username) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
-     * Opens a Hibernate Session with a Transaction.
-     * 
-     * @return A Session Object for the opened Session.
+     * Retrieves a user's login information from the database given the username.
+     * @param username The user's username.
+     * @return The user's login credentials.
      */
-    public Session openSessionwithTransaction() {
-        session = HibernateUtility.getSessionFactory().openSession();
-        transaction = session.beginTransaction();
-        return session;
-    }
-
-    public void closeSession() {
-        session.close();
-    }
-
-    public void closeSessionwithTransaction() {
-        transaction.commit();
-        session.close();
-    }
-
-    public Session getSession() {
-        return session;
-    }
-
-    public void setSession(Session session) {
-        this.session = session;
-    }
-
-    public Transaction getTransaction() {
-        return transaction;
-    }
-
-    public void setTransaction(Transaction transaction) {
-        this.transaction = transaction;
-    }
-
-    @SuppressWarnings("unchecked")
-    public boolean searchByUsername(String username) {
-        List<UserLogin> logins = (List<UserLogin>) getSession().createQuery("Select login From user_login Where login.username = :uname")
-                .setParameter("uname", username).getResultList();
-        if (logins.size() == 0) {
-            return false;
-        } else if (logins.get(0).getUsername() == username) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     @SuppressWarnings("unchecked")
     public UserLogin findByUsername(String username) {
-        List<UserLogin> logins = (List<UserLogin>) getSession().createQuery("Select login From user_login Where login.username = :uname")
+        List<UserLogin> logins = (List<UserLogin>) getSession()
+                .createQuery("Select login From user_login Where login.username = :uname")
                 .setParameter("uname", username).list();
         if (logins.size() > 0) {
             if (logins.get(0).getUsername() == username) {
@@ -86,6 +50,21 @@ public class UserLoginDAO implements DAOInteface<UserLogin> {
             }
         }
         return null;
+    }
+
+    /**
+     * Authenticates a user for the given username and password. The password parameter is
+     * in plaintext, but it is stored in the database as an encrypted string.
+     * @param username The username of the user.
+     * @param password The plaintext password of the user.
+     * @return A boolean value for whether the user is authenticated.
+     */
+    public boolean authenicateUser(String username, String password) {
+        UserLogin login = findByUsername(username);
+        if (login == null) {
+            return false;
+        }
+        return login.checkPassword(password);
     }
 
     public void persist(UserLogin entity) {
@@ -102,17 +81,15 @@ public class UserLoginDAO implements DAOInteface<UserLogin> {
 
     @SuppressWarnings("unchecked")
     public List<UserLogin> findAll() {
-        List<UserLogin> logins = (List<UserLogin>) getSession().createQuery("from user_login").list();
+        List<UserLogin> logins = (List<UserLogin>) getSession().createQuery("From user_login").list();
         return logins;
     }
 
-    /**
-     * Used to delete all information from the user_login table.
-     */
     public void deleteAll() {
         List<UserLogin> entityList = findAll();
         for (UserLogin entity : entityList) {
             delete(entity);
         }
     }
+
 }
