bf52c32088de8f31c06f681596cea20d1346356c
srichs <sr@srich.us>
2020-11-22 02:07:59 -0500

    Updated enum for status

--- src/main/java/edu/umgc/librarymanager/data/model/item/ItemStatus.java
+++ src/main/java/edu/umgc/librarymanager/data/model/item/ItemStatus.java
@@ -15,18 +15,60 @@ public enum ItemStatus {
     /**
      * Item is available for checkout.
      */
-    Available,
+    AVAILABLE,
     /**
      * Item has been reserved by another user.
      */
-    OnHold,
+    ON_HOLD,
     /**
      * Item has been checked out.
      */
-    CheckedOut,
+    CHECKED_OUT,
     /**
      * The item is in transit.
      */
-    InTransit
+    IN_TRANSIT;
+
+    public ItemStatus getItemStatus() {
+        return this;
+    }
+
+    /**
+     * Returns the ItemStatus based on an inputted String parameter.
+     * @param type The ItemStatus String
+     * @return The ItemStatus value of the String.
+     */
+    public static ItemStatus stringToItemStatus(String type) {
+        switch (type) {
+            case "Available":
+                return AVAILABLE;
+            case "On Hold":
+                return ON_HOLD;
+            case "Checked Out":
+                return CHECKED_OUT;
+            case "In Transit":
+                return IN_TRANSIT;
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * ToString Method of the Enum Type.
+     */
+    public String toString() {
+        switch (this) {
+            case AVAILABLE:
+                return "Available";
+            case ON_HOLD:
+                return "On Hold";
+            case CHECKED_OUT:
+                return "Checked Out";
+            case IN_TRANSIT:
+                return "In Transit";
+            default:
+                return "";
+        }
+    }
 
 }
