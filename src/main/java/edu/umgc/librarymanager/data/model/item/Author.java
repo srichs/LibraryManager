310a482871809f79ed7c336d3e929730cb9c138c
srichs <sr@srich.us>
2020-11-22 14:20:26 -0500

    Simplified class

--- src/main/java/edu/umgc/librarymanager/data/model/item/Author.java
+++ src/main/java/edu/umgc/librarymanager/data/model/item/Author.java
@@ -28,17 +28,8 @@ public class Author {
     @Column(name = "author_id")
     private long id;
 
-    @Column(name = "first_name")
-    private String firstName;
-
-    @Column(name = "middle_name")
-    private String middleName;
-
-    @Column(name = "last_name")
-    private String lastName;
-
-    @Column(name = "suffix")
-    private String suffix;
+    @Column(name = "name")
+    private String name;
 
     @ManyToMany(mappedBy = "author")
     private List<BaseItem> book;
@@ -47,106 +38,27 @@ public class Author {
      * The default constructor for the class.
      */
     public Author() {
-        this.firstName = "";
-        this.middleName = "";
-        this.lastName = "";
-        this.suffix = "";
+        this.name = "";
     }
 
     /**
-     * A constructor that takes a first and last name as parameters.
-     * @param fName The first name of the author.
-     * @param lName The last name of the author.
+     * A constructor that takes an author's name as a parameter.
+     * @param name The name of the author.
      */
-    public Author(String fName, String lName) {
-        this.firstName = fName;
-        this.middleName = "";
-        this.lastName = lName;
-        this.suffix = "";
+    public Author(String name) {
+        this.name = name;
     }
 
-    /**
-     * A constructor that takes a first, middle, and last name as parameters.
-     * @param fName The first name of the author.
-     * @param mName The middle name of the author.
-     * @param lName The last name of the author.
-     */
-    public Author(String fName, String mName, String lName) {
-        this.firstName = fName;
-        this.middleName = mName;
-        this.lastName = lName;
-        this.suffix = "";
+    public String getName() {
+        return this.name;
     }
 
-    /**
-     * A constructor with a parameter for each field.
-     * @param fName The first name of the author.
-     * @param mName The middle name of the author.
-     * @param lName The last name of the author.
-     * @param suffix The suffix of the author.
-     */
-    public Author(String fName, String mName, String lName, String suffix) {
-        this.firstName = fName;
-        this.middleName = mName;
-        this.lastName = lName;
-        this.suffix = suffix;
-    }
-
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public void setFirstName(String fName) {
-        this.firstName = fName;
-    }
-
-    public String getMiddleName() {
-        return this.middleName;
-    }
-
-    public void setMiddleName(String mName) {
-        this.middleName = mName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public void setLastName(String lName) {
-        this.lastName = lName;
-    }
-
-    public String getSuffix() {
-        return this.suffix;
-    }
-
-    public void setSuffix(String suffix) {
-        this.suffix = suffix;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public List<BaseItem> getBook() {
         return this.book;
-    }
-
-    /**
-     * The toString method of the class.
-     */
-    public String toString() {
-        String name = "";
-        boolean hasMiddle = !"".equals(this.middleName);
-        boolean hasLast = !"".equals(this.lastName);
-        boolean hasSuffix = !"".equals(this.suffix);
-        name = name + this.firstName;
-        if (hasMiddle) {
-            name = name + " " + this.middleName;
-        }
-        if (hasLast) {
-            name = name + " " + this.lastName;
-        }
-        if (hasSuffix) {
-            name = name + " " + this.suffix;
-        }
-        return name;
     }
 
 }
