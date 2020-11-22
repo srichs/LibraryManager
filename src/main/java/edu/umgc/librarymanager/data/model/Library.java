0a60ea1b8cdcd34651cfd8afcdeefb6280a7a159
srichs <sr@srich.us>
2020-11-22 18:38:34 -0500

    Updated file with fileds and annotations

--- src/main/java/edu/umgc/librarymanager/data/model/Library.java
+++ src/main/java/edu/umgc/librarymanager/data/model/Library.java
@@ -6,22 +6,67 @@
 
 package edu.umgc.librarymanager.data.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * A class that models some basic information for the Library.
  * @author David
  */
+@Entity
+@Table(name = "library")
 public class Library {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "library_id")
+    private long id;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone")
+    private String phone;
+
+    public Library() {
+        this.address = "";
+        this.name = "";
+        this.phone = "";
+    }
+
     public String getAddress() {
-        return null;
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getName() {
-        return null;
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getPhoneNumber() {
-        return null;
+        return this.phone;
+    }
+
+    public void setPhoneNumber(String phone) {
+        this.phone = phone;
+    }
+
+    public String toString() {
+        return this.name + "\n" + this.address + "\n" + this.phone;
     }
 
 }
