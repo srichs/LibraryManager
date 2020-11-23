1f373507df35b02bd2d6a6e5c7ee1f9d90bed15b
srichs <sr@srich.us>
2020-11-22 21:35:33 -0500

    Added method for testing database functions

--- src/main/java/edu/umgc/librarymanager/App.java
+++ src/main/java/edu/umgc/librarymanager/App.java
@@ -6,10 +6,13 @@
 
 package edu.umgc.librarymanager;
 
-import edu.umgc.librarymanager.data.access.DeweyCategoryDAO;
-import edu.umgc.librarymanager.data.access.HibernateUtility;
-import edu.umgc.librarymanager.data.model.item.DeweyCategory;
 import java.util.List;
+import edu.umgc.librarymanager.data.access.DeweyCategoryDAO;
+import edu.umgc.librarymanager.data.access.HibernateInit;
+import edu.umgc.librarymanager.data.access.HibernateUtility;
+import edu.umgc.librarymanager.data.access.UserDAO;
+import edu.umgc.librarymanager.data.model.item.DeweyCategory;
+import edu.umgc.librarymanager.data.model.user.BaseUser;
 
 /**
  * The main class of the application.
@@ -29,21 +32,27 @@ public final class App {
     }
 
     /**
-     * Practice method.
+     * Loads the database with Dewey Decimal categories and User information.
      */
     public static void databaseTest() {
-        DeweyCategory dewey = new DeweyCategory("004", "Computer science");
+        HibernateInit.initHibernate();
+
         DeweyCategoryDAO deweyDAO = new DeweyCategoryDAO();
-        deweyDAO.openSessionwithTransaction();
-        deweyDAO.persist(dewey);
-
-        DeweyCategory dewey2 = deweyDAO.findByCode("004");
-        System.out.println("\n" + dewey2.getCategory() + "\n");
-
+        DeweyCategory dewey = deweyDAO.findByCode("452");
         List<DeweyCategory> categories = deweyDAO.findAll();
-        System.out.println("\n" + categories.get(0).getCode() + " - " + categories.get(0).getCategory() + "\n");
+        System.out.println("\n" + dewey.getCategory() + "\n");
+        for (int i = 0; i < 31; i++) {
+            System.out.println(categories.get(i).getCategory());
+        }
+        System.out.println();
 
-        deweyDAO.closeSessionwithTransaction();
+        UserDAO userDAO = new UserDAO();
+        List<BaseUser> users = userDAO.findAll();
+        for (int i = 0; i < users.size(); i++) {
+            System.out.println(users.get(i).getLogin().getUsername() + " - " + users.get(i).getLogin().getPassword());
+        }
+        System.out.println();
+
         HibernateUtility.shutdown();
     }
 
