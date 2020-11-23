914456b13cb86e8dafded46a4d8217f42c755547
srichs <sr@srich.us>
2020-11-23 18:12:02 -0500

    Updated database test

--- src/main/java/edu/umgc/librarymanager/data/DatabaseTest.java
+++ src/main/java/edu/umgc/librarymanager/data/DatabaseTest.java
@@ -7,6 +7,7 @@
 package edu.umgc.librarymanager.data;
 
 import edu.umgc.librarymanager.data.access.DeweyCategoryDAO;
+import edu.umgc.librarymanager.data.access.HibernateUtility;
 import edu.umgc.librarymanager.data.access.ItemDAO;
 import edu.umgc.librarymanager.data.access.UserDAO;
 import edu.umgc.librarymanager.data.model.item.BaseBook;
@@ -28,6 +29,8 @@ public final class DatabaseTest {
      * Provides and example of retrieving information from the database.
      */
     public static void runTest() {
+        System.out.println(DatabaseTest.getHeader());
+        System.out.println(createHeader("Database Test"));
         // HibernateInit.initHibernate();
 
         // Retrieves the categories of Dewey Decimal then prints Codes 000 - 030
@@ -36,13 +39,14 @@ public final class DatabaseTest {
         DeweyCategory dewey = deweyDAO.findByCode("452");
         List<DeweyCategory> categories = deweyDAO.findAll();
         deweyDAO.closeSessionwithTransaction();
-        System.out.println("\n" + dewey.getCategory() + "\n");
+        System.out.println(createHeader("Dewey Categories"));
+        System.out.println("452 - " + dewey.getCategory() + "\n");
         for (int i = 0; i < 31; i++) {
             System.out.println(categories.get(i).getCode() + " - " + categories.get(i).getCategory());
         }
-        System.out.println();
 
         // Retrieves the User Information then prints the usernames and encrypted passwords
+        System.out.println(createHeader("User Information  -  Username and Encrypted Password"));
         UserDAO userDAO = new UserDAO();
         userDAO.openSessionwithTransaction();
         List<BaseUser> users = userDAO.findAll();
@@ -50,10 +54,10 @@ public final class DatabaseTest {
         for (int i = 0; i < users.size(); i++) {
             System.out.println(users.get(i).getLogin().getUsername() + " - " + users.get(i).getLogin().getPassword());
         }
-        System.out.println();
 
-        // Retrieves the Books in the database then prints the titles of index 0 - 30. Then it prints the dewey decimal 
+        // Retrieves the Books in the database then prints the titles of index 0 - 30. Then it prints the dewey decimal
         // code of index 23.
+        System.out.println(createHeader("Book Items  -  Titles"));
         ItemDAO itemDAO = new ItemDAO();
         itemDAO.openSessionwithTransaction();
         List<BaseItem> items = itemDAO.findAll();
@@ -65,7 +69,38 @@ public final class DatabaseTest {
         System.out.println(((BaseBook) items.get(23)).getClassificationCode().get(ClassType.DeweyDecimal).getCode());
         System.out.println();
 
-        // HibernateUtility.shutdown();
+        HibernateUtility.shutdown();
+        System.out.println(createHeader("Database Test Complete"));
+    }
+
+    /**
+     * Gets a String header that says "Library Manager".
+     * @return A String with the header.
+     */
+    public static String getHeader() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n---------------------------------------------------------------------------------------------\n");
+        sb.append("       __    _ __                             __  ___\n");
+        sb.append("      / /   (_) /_  _________  _______  __   /  |/  /___  ____  ____  ____  ___  _____\n");
+        sb.append("     / /   / / __ \\/ ___/ __ `/ ___/ / / /  / /|_/ / __ `/ __ \\/ __ `/ __ `/ _ \\/ ___/\n");
+        sb.append("    / /___/ / /_/ / /  / /_/ / /  / /_/ /  / /  / / /_/ / / / / /_/ / /_/ /  __/ /\n");
+        sb.append("   /_____/_/_.___/_/   \\__,_/_/   \\__, /  /_/  /_/\\__,_/_/ /_/\\__,_/\\__, /\\___/_/\n");
+        sb.append("                                 /____/                            /____/\n");
+        sb.append("\n---------------------------------------------------------------------------------------------\n");
+        return sb.toString();
+    }
+
+    /**
+     * Creates a header to be used in console.
+     * @param title The title to include in the header.
+     * @return The complete String header.
+     */
+    public static String createHeader(String title) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n---------------------------------------------------------------------------------------------\n");
+        sb.append("   " + title + " ");
+        sb.append("\n---------------------------------------------------------------------------------------------\n");
+        return sb.toString();
     }
 
 }
