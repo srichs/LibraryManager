669262e43da63afd3ebc48ce1db3ffdd1bfee1dc
srichs <sr@srich.us>
2020-11-25 22:56:12 -0500

    Minor updates to data access

--- src/main/java/edu/umgc/librarymanager/data/access/UserDAO.java
+++ src/main/java/edu/umgc/librarymanager/data/access/UserDAO.java
@@ -9,6 +9,7 @@ package edu.umgc.librarymanager.data.access;
 import edu.umgc.librarymanager.IUserService;
 import edu.umgc.librarymanager.data.model.user.BaseUser;
 import edu.umgc.librarymanager.data.model.user.IUser;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -36,6 +37,39 @@ public class UserDAO extends BaseDAO<BaseUser> implements IUserService {
     public List<BaseUser> findAll() {
         List<BaseUser> list = (List<BaseUser>) getSession().createQuery("From BaseUser").list();
         return list;
+    }
+
+    /**
+     * Creates a HashMap of users so they can be searched by username.
+     * @return A HashMap<String, BaseUser> where String is the username.
+     */
+    @SuppressWarnings("unchecked")
+    public HashMap<String, BaseUser> getUserHashMap() {
+        HashMap<String, BaseUser> map = new HashMap<String, BaseUser>();
+        List<BaseUser> list = (List<BaseUser>) getSession().createQuery("From BaseUser").list();
+        for (int i = 0; i < list.size(); i++) {
+            map.put(list.get(i).getLogin().getUsername(), list.get(i));
+        }
+        return map;
+    }
+
+    /**
+     * Searches for whether a user exists in the database.
+     * @param username The user's username.
+     * @return A user who was searched.
+     */
+    @SuppressWarnings("unchecked")
+    public BaseUser searchByUsername(String username) {
+        List<BaseUser> users = (List<BaseUser>) getSession()
+                .createQuery("From BaseUser bu Where bu.username = :uname")
+                .setParameter("uname", username).getResultList(); // TODO fix query
+        if (users.size() > 0) {
+            if (users.get(0).getLogin().getUsername() == username) {
+                System.out.println(users.get(0).getEmail());
+                return users.get(0);
+            }
+        }
+        return null;
     }
 
 }
