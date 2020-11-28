c72c16abd45b1fdd51c9e9fdc39793cff4897f50
srichs <sr@srich.us>
2020-11-28 18:08:50 -0500

    Added image icons

--- src/main/java/edu/umgc/librarymanager/gui/MainFrame.java
+++ src/main/java/edu/umgc/librarymanager/gui/MainFrame.java
@@ -2,16 +2,23 @@
  * Filename: MainFrame.java
  * Author: Scott
  * Date Created: 11/25/2020
+ * Icon Image From: https://www.flaticon.com/free-icon/open-book_171322?term=book&page=1&position=12
  */
 
 package edu.umgc.librarymanager.gui;
 
 import edu.umgc.librarymanager.data.access.HibernateUtility;
+import edu.umgc.librarymanager.gui.panels.PanelComposite;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -30,7 +37,7 @@ public class MainFrame extends JFrame {
     private PanelComposite panelComp;
 
     /**
-     * Constructor for the NVMainFrame Class.
+     * Constructor for the MainFrame Class.
      * @param control The GUIController to be the ActionListener.
      */
     public MainFrame(GUIController control) {
@@ -47,8 +54,55 @@ public class MainFrame extends JFrame {
                 System.exit(0);
             }
         });
-        //this.setIconImages(getIconList());
-        this.initComponents();
+        this.setIconImages(getIconList());
+        initComponents();
+    }
+
+    private void initComponents() {
+        this.menuBar = new MenuBar(control);
+        this.setJMenuBar(menuBar);
+        this.panels = new JPanel();
+        this.layout = new CardLayout();
+        this.panelComp = new PanelComposite(control);
+        this.panels.setLayout(this.layout);
+        initPanels();
+        this.getContentPane().add(this.panels, BorderLayout.CENTER);
+        this.layout.show(this.panels, PanelComposite.LOGIN);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+    private void initPanels() {
+        this.panels.add(this.panelComp.getLoginPanel(), PanelComposite.LOGIN);
+        this.panels.add(this.panelComp.getLibrarianMenuPanel(), PanelComposite.LIBRARIAN_MENU);
+        this.panels.add(this.panelComp.getPatronMenuPanel(), PanelComposite.PATRON_MENU);
+        this.panels.add(this.panelComp.getSearchPanel(), PanelComposite.SEARCH);
+        this.panels.add(this.panelComp.getUserProfilePanel(), PanelComposite.PROFILE);
+        this.panels.add(this.panelComp.getAddUserPanel(), PanelComposite.ADD_USER);
+    }
+
+    private List<Image> getIconList() {
+        List<Image> icons = new ArrayList<Image>();
+        icons.add(new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass()
+                .getResource("/icon/16.png"))).getImage());
+        icons.add(new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass()
+                .getResource("/icon/32.png"))).getImage());
+        icons.add(new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass()
+                .getResource("/icon/64.png"))).getImage());
+        icons.add(new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass()
+                .getResource("/icon/128.png"))).getImage());
+        icons.add(new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass()
+                .getResource("/icon/256.png"))).getImage());
+        return icons;
+    }
+
+    public MenuBar getTheMenuBar() {
+        return this.menuBar;
+    }
+
+    public void setTheMenuBar(MenuBar menuBar) {
+        this.menuBar = menuBar;
     }
 
     public JPanel getPanels() {
@@ -74,38 +128,5 @@ public class MainFrame extends JFrame {
     public void setPanelComp(PanelComposite panelComp) {
         this.panelComp = panelComp;
     }
-
-    private void initComponents() {
-        this.menuBar = new MenuBar(control);
-        this.setJMenuBar(menuBar);
-        this.panels = new JPanel();
-        this.layout = new CardLayout();
-        this.panelComp = new PanelComposite(control);
-        this.panels.setLayout(this.layout);
-        panels.add(panelComp.getLoginPanel(), AppGUI.LOGIN);
-        panels.add(panelComp.getBlankPanel(), AppGUI.BLANK);
-        this.getContentPane().add(this.panels, BorderLayout.CENTER);
-        this.layout.show(this.panels, AppGUI.LOGIN);
-        this.pack();
-        this.setVisible(true);
-        this.setLocationRelativeTo(null);
-    }
-
-    /*private List<Image> getIconList() {
-        // TODO add icon images
-        List<Image> icons = new ArrayList<Image>();
-        icons.add(new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass()
-                .getResource("./src/main/resources/data/"))).getImage());
-        icons.add(new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass()
-                .getResource("./src/main/resources/data/"))).getImage());
-        icons.add(new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass()
-                .getResource("./src/main/resources/data/"))).getImage());
-        icons.add(new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass()
-                .getResource("./src/main/resources/data/"))).getImage());
-        icons.add(new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass()
-                .getResource("./src/main/resources/data/"))).getImage());
-        return icons;
-        return null;
-    }*/
 
 }
