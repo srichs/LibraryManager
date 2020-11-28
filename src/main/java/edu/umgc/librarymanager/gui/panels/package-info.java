7b06d4ebf387650f819a3227dac210dc8385baee
srichs <sr@srich.us>
2020-11-28 18:09:48 -0500

    Added javadoc package info

---
+++ src/main/java/edu/umgc/librarymanager/gui/panels/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Filename: package-info.java
+ * Author: Scott
+ * Date Created: 11/27/2020
+ */
+
+/**
+ * This package contains the Panels that are used in the LibraryManager application.
+ */
+package edu.umgc.librarymanager.gui.panels;
