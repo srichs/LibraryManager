540710bba35afd9b4fac542b37a4cbb630b151dd
srichs <sr@srich.us>
2020-11-29 15:28:31 -0500

    Added panels for User management

--- src/main/java/edu/umgc/librarymanager/gui/panels/AllUsersPanel.java
+++ src/main/java/edu/umgc/librarymanager/gui/panels/AllUsersPanel.java
@@ -6,10 +6,174 @@
 
 package edu.umgc.librarymanager.gui.panels;
 
+import edu.umgc.librarymanager.data.model.user.BaseUser;
+import edu.umgc.librarymanager.gui.Command;
+import edu.umgc.librarymanager.gui.GUIController;
+import edu.umgc.librarymanager.service.ControlHelper;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
 /**
- * This class
+ * This class is used to view all users of the Library system.
  * @author Scott
  */
-public class AllUsersPanel {
-    
+public class AllUsersPanel extends JPanel implements ActionListener {
+
+    private static final long serialVersionUID = -9189700781888896926L;
+
+    private JScrollPane scrollPane;
+    private JPanel userPanel;
+    private JTextField searchField;
+    private JButton searchButton;
+    private JButton addUserButton;
+    private List<BaseUser> users;
+    private BaseUser selectedUser;
+    private GUIController control;
+
+    /**
+     * The constructor of the class.
+     * @param control The GUIController of the application.
+     */
+    public AllUsersPanel(GUIController control) {
+        this.users = null;
+        this.control = control;
+        createPanel(control);
+    }
+
+    /**
+     * Creates the panel to be displayed by the class.
+     */
+    private void createPanel(GUIController control) {
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        JPanel searchPanel = new JPanel(new FlowLayout());
+        searchPanel.setPreferredSize(new Dimension(880, 60));
+        searchPanel.setMinimumSize(new Dimension(880, 60));
+        searchPanel.setBorder(new EmptyBorder(new Insets(5, 5, 5, 5)));
+        this.searchField = new JTextField(28);
+        this.searchButton = new JButton("Search");
+        this.searchButton.addActionListener(this);
+        this.searchButton.setActionCommand(Command.SEARCH);
+        JButton clearButton = new JButton("Clear");
+        clearButton.addActionListener(this);
+        clearButton.setActionCommand(Command.CLEAR);
+        this.addUserButton = new JButton("Add User");
+        this.addUserButton.addActionListener(control);
+        this.addUserButton.setActionCommand(Command.ADD_USER);
+        searchPanel.add(new JLabel("Search by Lastname "));
+        searchPanel.add(this.searchField);
+        searchPanel.add(this.searchButton);
+        searchPanel.add(clearButton);
+        searchPanel.add(Box.createRigidArea(new Dimension(100, 40)));
+        searchPanel.add(this.addUserButton);
+        mainPanel.add(searchPanel, BorderLayout.NORTH);
+
+        this.userPanel = new JPanel();
+        this.userPanel.setLayout(new BoxLayout(this.userPanel, BoxLayout.Y_AXIS));
+        this.scrollPane = new JScrollPane(this.userPanel);
+        this.scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+        fillPane(this.users);
+
+        this.scrollPane.revalidate();
+        this.scrollPane.repaint();
+        mainPanel.add(this.scrollPane);
+        this.setLayout(new BorderLayout());
+        this.add(mainPanel, BorderLayout.CENTER);
+        this.setVisible(true);
+    }
+
+    private void fillPane(List<BaseUser> list) {
+        if (list == null) {
+            return;
+        }
+        this.userPanel.removeAll();
+        for (int i = 0; i < list.size(); i++) {
+            UserView view = new UserView(list.get(i), new UserActionListener(list.get(i)));
+            this.userPanel.add(view);
+        }
+        this.scrollPane.revalidate();
+        this.scrollPane.repaint();
+    }
+
+    private void searchList(String lastName) {
+        List<BaseUser> newList = new ArrayList<BaseUser>();
+        for (int i = 0; i < this.users.size(); i++) {
+            String lname = this.users.get(i).getLastName();
+            if (lname.toLowerCase().contains(lastName.toLowerCase())) {
+                newList.add(this.users.get(i));
+            }
+        }
+        fillPane(newList);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (Command.SEARCH.equals(e.getActionCommand())) {
+            if (!this.searchField.getText().equals("")) {
+                searchList(this.searchField.getText());
+            }
+        } else if (Command.CLEAR.equals(e.getActionCommand())) {
+            this.searchField.setText("");
+            fillPane(this.users);
+        }
+    }
+
+    /**
+     * Sets the users list of the class and clears the search panel then fills the scroll pane.
+     * @param users The List of users of the system.
+     */
+    public void setUsers(List<BaseUser> users) {
+        this.users = users;
+        this.searchField.setText("");
+        fillPane(users);
+    }
+
+    public BaseUser getSelectedUser() {
+        return this.selectedUser;
+    }
+
+    public void setSelectedUser(BaseUser user) {
+        this.selectedUser = user;
+    }
+
+    public GUIController getController() {
+        return this.control;
+    }
+
+    /**
+     * An inner class to listen to the users in the scroll pane and fire actions based on which
+     * button is pressed in a user's panel.
+     * @author Scott
+     */
+    private class UserActionListener implements ActionListener {
+        private BaseUser user;
+
+        UserActionListener(BaseUser user) {
+            this.user = user;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            setSelectedUser(this.user);
+            if (Command.VIEW_USER.equals(e.getActionCommand())) {
+                ControlHelper.viewUser(getController().getFrame(), this.user);
+            } else if (Command.DELETE_USER.equals(e.getActionCommand())) {
+                ControlHelper.deleteUser(getController(), this.user);
+            }
+        }
+    }
+
 }
---
+++ src/main/java/edu/umgc/librarymanager/gui/panels/EditUserPanel.java
@@ -0,0 +1,242 @@
+/*
+ * Filename: EditUserPanel.java
+ * Author: Scott
+ * Date Created: 11/29/2020
+ */
+
+package edu.umgc.librarymanager.gui.panels;
+
+import edu.umgc.librarymanager.data.model.user.BaseUser;
+import edu.umgc.librarymanager.gui.Command;
+import edu.umgc.librarymanager.gui.DialogUtil;
+import edu.umgc.librarymanager.gui.GUIController;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * This class is used to edit a user to the database.
+ * @author Scott
+ */
+public class EditUserPanel extends JPanel {
+
+    private static final long serialVersionUID = 8571310030746565474L;
+
+    private LabelFieldPanel idPanel;
+    private LabelFieldPanel createDatePanel;
+    private LabelFieldPanel usernamePanel;
+    private LabelFieldPanel firstNamePanel;
+    private LabelFieldPanel lastNamePanel;
+    private LabelFieldPanel emailPanel;
+    private LabelFieldPanel addressPanel;
+    private LabelFieldPanel phonePanel;
+    private LabelFieldPanel typePanel;
+    private JPasswordField password1;
+    private JPasswordField password2;
+    private JButton button;
+    private BaseUser user;
+
+    /**
+     * The constructor of the class.
+     * @param control The GUIController to act as the listener.
+     */
+    public EditUserPanel(GUIController control) {
+        super();
+        this.idPanel = new LabelFieldPanel();
+        this.createDatePanel = new LabelFieldPanel();
+        this.usernamePanel = new LabelFieldPanel();
+        this.firstNamePanel = new LabelFieldPanel();
+        this.lastNamePanel = new LabelFieldPanel();
+        this.emailPanel = new LabelFieldPanel();
+        this.addressPanel = new LabelFieldPanel();
+        this.phonePanel = new LabelFieldPanel();
+        this.typePanel = new LabelFieldPanel();
+        this.password1 = new JPasswordField();
+        this.password2 = new JPasswordField();
+        this.password1.setColumns(30);
+        this.password2.setColumns(30);
+        this.button = new JButton("Update");
+        createPanel(control);
+    }
+
+    public void setUser(BaseUser user) {
+        this.user = user;
+        setUserInfo();
+    }
+
+    /**
+     * Sets the users information to the form fields.
+     * @param user The user to set the information from.
+     */
+    private void setUserInfo() {
+        if (user == null) {
+            return;
+        }
+        this.idPanel.setText(String.valueOf(user.getId()));
+        this.createDatePanel.setText(user.createdDateTime().toString());
+        this.usernamePanel.setText(user.getUserName());
+        this.firstNamePanel.setText(user.getFirstName());
+        this.lastNamePanel.setText(user.getLastName());
+        this.emailPanel.setText(user.getEmail());
+        this.addressPanel.setText(user.getAddress());
+        this.phonePanel.setText(user.getPhoneNumber());
+        this.typePanel.setText(user.getUserType().toString());
+    }
+
+    private void createPanel(GUIController control) {
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setSize(new Dimension(400, 400));
+        mainPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
+        JPanel fieldPanel = new JPanel();
+        fieldPanel.setLayout(new BoxLayout(fieldPanel, BoxLayout.Y_AXIS));
+        fieldPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        addPanel(this.idPanel, fieldPanel, "ID", "auto", true);
+        addPanel(this.createDatePanel, fieldPanel, "Date Created", "auto", true);
+        addPanel(this.usernamePanel, fieldPanel, "Username", "auto", false);
+        passwordPanel(this.password1, fieldPanel);
+        passwordPanel(this.password2, fieldPanel);
+        addPanel(this.firstNamePanel, fieldPanel, "First Name", "", false);
+        addPanel(this.lastNamePanel, fieldPanel, "Last Name", "", false);
+        addPanel(this.emailPanel, fieldPanel, "Email", "", false);
+        addPanel(this.addressPanel, fieldPanel, "Address", "", false);
+        addPanel(this.phonePanel, fieldPanel, "Phone", "", false);
+        addPanel(this.typePanel, fieldPanel, "User Type", "auto", true); // TODO change to combo box?
+        addButton(this.button, fieldPanel, Command.MANAGE_UPDATE_USER, control);
+        mainPanel.add(fieldPanel, BorderLayout.CENTER);
+        this.setLayout(new FlowLayout());
+        this.add(mainPanel);
+        this.setVisible(true);
+    }
+
+    private void passwordPanel(JPasswordField passField, Container container) {
+        JLabel label = new JLabel("Password");
+        label.setPreferredSize(new Dimension(76, 20));
+        JPanel mainPanel = new JPanel(new GridBagLayout());
+        mainPanel.setSize(new Dimension(400, 60));
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(5, 10, 5, 10);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.EAST;
+        c.gridx = 0;
+        c.weightx = 0.2;
+        c.gridwidth = 1;
+        c.gridy = 0;
+        mainPanel.add(label, c);
+        c.anchor = GridBagConstraints.CENTER;
+        c.gridx = 1;
+        c.weightx = 0.8;
+        c.gridwidth = 2;
+        c.gridy = 0;
+        mainPanel.add(passField, c);
+        container.add(mainPanel);
+    }
+
+    private static void addPanel(LabelFieldPanel panel, Container container, String label, String textField,
+            boolean readOnly) {
+        panel.setLabel(label);
+        panel.setText(textField);
+        panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        container.add(panel);
+        if (readOnly) {
+            panel.getTextField().setEditable(false);
+        }
+    }
+
+    private static void addButton(JButton button, Container container, String command, GUIController control) {
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.setActionCommand(command);
+        button.addActionListener((ActionListener) control);
+        container.add(Box.createRigidArea(new Dimension(20, 20)));
+        container.add(button);
+    }
+
+    /**
+     * Tries to create a new user with the information in the form and returns the user if created.
+     * @return The BaseUser if added or null if not added.
+     */
+    public BaseUser tryUpdate() {
+        if (checkFields()) {
+            if (checkInfoDiff(user)) {
+                BaseUser updatedUser = user;
+                updatedUser.setEmail(this.emailPanel.getText());
+                updatedUser.setAddress(this.addressPanel.getText());
+                updatedUser.setPhoneNumber(this.phonePanel.getText());
+                setNew();
+                return updatedUser;
+            }
+        }
+        return null;
+    }
+
+    private boolean checkInfoDiff(BaseUser user) {
+        boolean passwordChanged = false;
+        if (this.password1.getPassword().length > 0 && this.password2.getPassword().length > 0) {
+            if (!user.getLogin().checkPassword(this.password1.getPassword())) {
+                passwordChanged = true;
+            }
+        }
+        if (this.usernamePanel.getTextField().getText().equals(user.getUserName())
+                && this.firstNamePanel.getTextField().getText().equals(user.getFirstName())
+                && this.lastNamePanel.getTextField().getText().equals(user.getLastName())
+                && this.emailPanel.getTextField().getText().equals(user.getEmail())
+                && this.addressPanel.getTextField().getText().equals(user.getAddress())
+                && this.phonePanel.getTextField().getText().equals(user.getPhoneNumber())
+                && !passwordChanged) {
+            DialogUtil.informationMessage("No update was necessary.", "Update Information");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkFields() {
+        if (this.password1.getPassword().length > 0 || this.password2.getPassword().length > 0) {
+            if (this.password1.getPassword().length < 8) {
+                DialogUtil.warningMessage("The password should be 8 characters or longer.", "Update Failure");
+                return false;
+            }
+            if (!Arrays.equals(this.password1.getPassword(), this.password2.getPassword())) {
+                DialogUtil.warningMessage("The passwords do not match.", "Update Failure");
+                return false;
+            }
+        }
+        if (this.usernamePanel.getTextField().getText().equals("")
+                || this.firstNamePanel.getTextField().getText().equals("")
+                || this.lastNamePanel.getTextField().getText().equals("")
+                || this.emailPanel.getTextField().getText().equals("")
+                || this.addressPanel.getTextField().getText().equals("")
+                || this.phonePanel.getTextField().getText().equals("")) {
+            DialogUtil.warningMessage("All fields must be filled.", "Update Failure");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Sets all of the editable fields blank.
+     */
+    public void setNew() {
+        this.usernamePanel.setText("");
+        this.password1.setText("");
+        this.password2.setText("");
+        this.firstNamePanel.setText("");
+        this.lastNamePanel.setText("");
+        this.emailPanel.setText("");
+        this.addressPanel.setText("");
+        this.phonePanel.setText("");
+    }
+
+}
