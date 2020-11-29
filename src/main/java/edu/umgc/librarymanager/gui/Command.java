c22d16505fe80be1adab1efcf4b7b9d98602040d
srichs <sr@srich.us>
2020-11-29 15:25:12 -0500

    Added class for constants for ActionCommands

---
+++ src/main/java/edu/umgc/librarymanager/gui/Command.java
@@ -0,0 +1,106 @@
+/*
+ * Filename: Command.java
+ * Author: Scott
+ * Date Created: 11/29/2020
+ */
+
+package edu.umgc.librarymanager.gui;
+
+/**
+ * This is a utility class that provides names for Action Commands.
+ * @author Scott
+ */
+public final class Command {
+
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String LOGIN = "login";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String LOGOUT = "logout";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String ABOUT = "about";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String HELP = "help";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String ADD_USER = "add_user";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String CREATE_USER = "create_user";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String REMOVE_USER = "remove_user";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String UPDATE_USER = "update_user";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String MANAGE_USERS = "manage_users";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String VIEW_USER = "view_user";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String DELETE_USER = "delete_user";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String MANAGE_UPDATE_USER = "manage_update";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String ADD_ITEM = "add_item";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String REMOVE_ITEM = "remove_item";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String CHECKOUT_ITEM = "checkout_item";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String RETURN_ITEM = "return_item";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String SEARCH = "search";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String CLEAR = "clear";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String CHECKED_ITEMS = "checked_items";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String PROFILE = "profile";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String LIBRARIAN_MENU = "librarian_menu";
+    /**
+     * A string for an ActionCommand.
+     */
+    public static final String PATRON_MENU = "patron_menu";
+
+    private Command() {}
+
+}
