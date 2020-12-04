d76aeefcb67b936baec9b250692e1bc6921c0dba
srichs <sr@srich.us>
2020-12-03 19:32:32 -0500

    Added Pagination and minor updates

--- src/main/java/edu/umgc/librarymanager/data/access/ItemDAO.java
+++ src/main/java/edu/umgc/librarymanager/data/access/ItemDAO.java
@@ -8,6 +8,7 @@ package edu.umgc.librarymanager.data.access;
 
 import edu.umgc.librarymanager.data.model.item.BaseItem;
 import java.util.List;
+import org.hibernate.query.Query;
 
 /**
  * This class is the Data Access Object class for the BaseItem class.
@@ -21,18 +22,16 @@ public class ItemDAO extends BaseDAO<BaseItem> {
 
     /**
      * Retrieves an item's information from the database given the title.
-     * @param title The item's title.
-     * @return The item's information.
+     * @param searchTerm The seach terms to be search for in the title field.
+     * @return The items matching the search terms information.
      */
     @SuppressWarnings("unchecked")
-    public BaseItem findByTitle(String title) {
+    public List<BaseItem> searchByTitle(String searchTerm) {
         List<BaseItem> items = (List<BaseItem>) getSession()
-                .createQuery("From BaseItem i Where i.title = :title")
-                .setParameter("title", title).list();
+                .createQuery("From BaseItem i Where i.title like :searchTerm")
+                .setParameter("searchTerm", "%" + searchTerm + "%").getResultList();
         if (items.size() > 0) {
-            if (items.get(0).getTitle() == title) {
-                return items.get(0);
-            }
+            return items;
         }
         return null;
     }
@@ -40,8 +39,24 @@ public class ItemDAO extends BaseDAO<BaseItem> {
     @Override
     @SuppressWarnings("unchecked")
     public List<BaseItem> findAll() {
-        List<BaseItem> list = (List<BaseItem>) getSession().createQuery("From BaseItem").list();
+        List<BaseItem> list = (List<BaseItem>) getSession().createQuery("From BaseItem").getResultList();
         return list;
+    }
+
+    /**
+     * Retrieves a List of BaseItems for the given pagination parameters.
+     * @param pagination The Pagination parameters to get the results for.
+     * @return The List of BaseItems for the given pagination parameters.
+     */
+    @SuppressWarnings("unchecked")
+    public List<BaseItem> findAllPaginated(Pagination pagination) {
+        Query<Integer> countQuery = getSession().createQuery("Select Count (i.id) From BaseItem i");
+        pagination.setTotalCount((Integer) countQuery.uniqueResult());
+        Query<BaseItem> selectQuery = getSession().createQuery("From BaseItem");
+        selectQuery.setFirstResult((pagination.getDesiredPage() - 1) * pagination.getPageSize());
+        selectQuery.setMaxResults(pagination.getPageSize());
+        List<BaseItem> page = selectQuery.getResultList();
+        return page;
     }
 
 }
