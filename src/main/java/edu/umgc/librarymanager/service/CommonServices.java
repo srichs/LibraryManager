9233bb1d69213b68e7acc99e05d9ae55cb11b36b
srichs <sr@srich.us>
2020-12-08 14:46:04 -0500

    Simplified login for users from db

--- src/main/java/edu/umgc/librarymanager/service/CommonServices.java
+++ src/main/java/edu/umgc/librarymanager/service/CommonServices.java
@@ -13,9 +13,9 @@ import edu.umgc.librarymanager.gui.DialogUtil;
 import edu.umgc.librarymanager.gui.GUIController;
 import edu.umgc.librarymanager.gui.InactivityListener;
 import edu.umgc.librarymanager.gui.panels.PanelComposite;
-import java.util.HashMap;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.HibernateException;
 
 /**
  * This class is used to provide helper methods for the GUIController class.
@@ -33,35 +33,42 @@ public final class CommonServices {
      */
     public static void login(GUIController control) {
         String username = control.getFrame().getPanelComp().getLoginPanel().getUsernameField().getText();
-        HashMap<String, BaseUser> map = control.getLogins();
+        UserDAO userDAO = new UserDAO();
         BaseUser user = null;
-        if (map.containsKey(username)) {
-            if (map.get(username).getLogin().checkPassword(control.getFrame().getPanelComp().getLoginPanel()
+        try {
+            userDAO.openSessionwithTransaction();
+            user = userDAO.searchByUsername(username);
+            userDAO.closeSessionwithTransaction();
+        } catch (HibernateException ex) {
+            ex.printStackTrace();
+        } finally {
+            userDAO.closeSession();
+        }
+        if (user != null) {
+            if (user.getLogin().checkPassword(control.getFrame().getPanelComp().getLoginPanel()
                     .getPasswordField().getPassword())) {
                 control.getFrame().getPanelComp().getLoginPanel().clearFields();
-                user = map.get(username);
+                control.setCurrentUser(user);
+                LOG.info(user.getUserName() + " has logged on.");
+                if (control.getCurrentUser().getUserType() == UserType.Librarian) {
+                    control.getFrame().getTheMenuBar().setLibrarianMenuBar(control);
+                    control.getFrame().getLayout().show(control.getFrame().getPanels(), PanelComposite.LIBRARIAN_MENU);
+                } else {
+                    control.getFrame().getTheMenuBar().setPatronMenuBar(control);
+                    control.getFrame().getPanelComp().getSearchPanel()
+                            .setFirstName(control.getCurrentUser().getFirstName());
+                    control.getFrame().getPanelComp().getSearchPanel().getSearchField().setText("");
+                    control.getFrame().getLayout().show(control.getFrame().getPanels(), PanelComposite.SEARCH);
+                }
+                control.getFrame().repaint();
+                // user is logged out after 5 min of inactivity
+                control.setInactiveListener(new InactivityListener(control.getFrame(), control.getLogoutAction(), 5));
+                control.getInactiveListener().start();
             } else {
                 DialogUtil.warningMessage("Incorrect password. Access denied.", "Login Failure");
             }
         } else {
             DialogUtil.warningMessage("The username was not recognized.", "Login Failure");
-        }
-        if (user != null) {
-            control.setCurrentUser(user);
-            LOG.info(user.getUserName() + " has logged on.");
-            if (control.getCurrentUser().getUserType() == UserType.Librarian) {
-                control.getFrame().getTheMenuBar().setLibrarianMenuBar(control);
-                control.getFrame().getLayout().show(control.getFrame().getPanels(), PanelComposite.LIBRARIAN_MENU);
-            } else {
-                control.getFrame().getTheMenuBar().setPatronMenuBar(control);
-                control.getFrame().getPanelComp().getSearchPanel()
-                        .setFirstName(control.getCurrentUser().getFirstName());
-                control.getFrame().getLayout().show(control.getFrame().getPanels(), PanelComposite.SEARCH);
-            }
-            control.getFrame().repaint();
-            // user is logged out after 5 min of inactivity
-            control.setInactiveListener(new InactivityListener(control.getFrame(), control.getLogoutAction(), 5));
-            control.getInactiveListener().start();
         }
     }
 
@@ -85,19 +92,6 @@ public final class CommonServices {
             control.getInactiveListener().stop();
             control.setInactiveListener(null);
         }
-    }
-
-    /**
-     * Gets a hashmap of login credentials from the database.
-     * @return A HashMap with the username as the key.
-     */
-    public static HashMap<String, BaseUser> getLoginCredentials() {
-        HashMap<String, BaseUser> map = new HashMap<String, BaseUser>();
-        UserDAO userDAO = new UserDAO();
-        userDAO.openSessionwithTransaction();
-        map = userDAO.getUserHashMap();
-        userDAO.closeSessionwithTransaction();
-        return map;
     }
 
 }
