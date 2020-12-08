474b09b0e9c1e86938c00798985773ed1750a6a7
srichs <sr@srich.us>
2020-12-08 14:44:49 -0500

    Added different types of ItemViews

--- src/main/java/edu/umgc/librarymanager/gui/panels/ItemView.java
+++ src/main/java/edu/umgc/librarymanager/gui/panels/ItemView.java
@@ -7,9 +7,12 @@
 package edu.umgc.librarymanager.gui.panels;
 
 import edu.umgc.librarymanager.data.model.item.BaseItem;
+import edu.umgc.librarymanager.data.model.item.ItemStatus;
 import edu.umgc.librarymanager.gui.Command;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Insets;
@@ -32,15 +35,29 @@ public class ItemView extends JPanel {
 
     private static final long serialVersionUID = 1L;
 
+    private ItemViewType type;
     private BaseItem item;
 
-    public ItemView(ActionListener listener) {
+    /**
+     * A constructor for the class.
+     * @param type The ItemViewType to determine where the ItemView is being displayed.
+     * @param listener The ActionListener that will implement the actions performed with the buttons.
+     */
+    public ItemView(ItemViewType type, ActionListener listener) {
         this.item = null;
+        this.type = type;
         createPanel(listener);
     }
 
-    public ItemView(BaseItem item, ActionListener listener) {
+    /**
+     * A constructor for the class.
+     * @param item The BaseItem that is associated with the View.
+     * @param type The ItemViewType to determine where the ItemView is being displayed.
+     * @param listener The ActionListener that will implement the actions performed with the buttons.
+     */
+    public ItemView(BaseItem item, ItemViewType type, ActionListener listener) {
         this.item = item;
+        this.type = type;
         createPanel(listener);
     }
 
@@ -51,26 +68,11 @@ public class ItemView extends JPanel {
         rightPanel.setBorder(new EmptyBorder(new Insets(5, 5, 5, 5)));
         rightPanel.setPreferredSize(new Dimension(120, 70));
         rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
-        JButton viewButton = new JButton("View");
-        viewButton.addActionListener(listener);
-        viewButton.setActionCommand(Command.VIEW_ITEM);
-        viewButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        JButton delButton = new JButton("Delete");
-        delButton.addActionListener(listener);
-        delButton.setActionCommand(Command.DELETE_ITEM);
-        delButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        rightPanel.add(viewButton);
-        rightPanel.add(Box.createRigidArea(new Dimension(5, 5)));
-        rightPanel.add(delButton);
+        createButtonPanel(rightPanel, listener);
         leftPanel.setLayout(new GridLayout(3, 2));
         leftPanel.setBorder(new EmptyBorder(new Insets(5, 20, 5, 5)));
         leftPanel.add(new JLabel("Title: " + item.getTitle())); // TODO make view more detailed
-        /*leftPanel.add(new JLabel("Email: " + user.getEmail()));
-        leftPanel.add(new JLabel("User Type: " + user.getUserType().toString()));
-        leftPanel.add(new JLabel("Username: " + user.getUserName()));
-        leftPanel.add(new JLabel("Phone: " + user.getPhoneNumber()));
-        leftPanel.add(new JLabel("Address: " + user.getAddress()));
-        leftPanel.add(new JLabel("ID: " + user.getId()));*/
+        /*leftPanel.add(new JLabel("Email: " + user.getEmail()));*/
         this.setPreferredSize(new Dimension(860, 80));
         this.setMaximumSize(new Dimension(860, 80));
         this.setLayout(new BorderLayout());
@@ -78,6 +80,61 @@ public class ItemView extends JPanel {
         this.add(leftPanel, BorderLayout.WEST);
         this.add(rightPanel, BorderLayout.EAST);
         this.setVisible(true);
+    }
+
+    private void createButtonPanel(Container container, ActionListener listener) {
+        JButton firstBtn = new JButton();
+        JLabel label = new JLabel();
+        JButton secondBtn = new JButton();
+        int btnNumber = 2;
+        if (this.type == ItemViewType.View) {
+            firstBtn.setText(ItemViewType.View.toString());
+            firstBtn.addActionListener(listener);
+            firstBtn.setActionCommand(Command.VIEW_ITEM);
+            firstBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+            secondBtn.setText("Delete");
+            secondBtn.addActionListener(listener);
+            secondBtn.setActionCommand(Command.DELETE_ITEM);
+            secondBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        } else if (this.type == ItemViewType.Renew) {
+            btnNumber = 1;
+            firstBtn.setText(ItemViewType.Renew.toString());
+            firstBtn.addActionListener(listener);
+            firstBtn.setActionCommand(Command.ITEM_RENEWED); // TODO
+            firstBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        } else if (this.type == ItemViewType.Return_) {
+            btnNumber = 1;
+            firstBtn.setText(ItemViewType.Return_.toString());
+            firstBtn.addActionListener(listener);
+            firstBtn.setActionCommand(Command.ITEM_RETURNED); // TODO
+            firstBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        } else if (this.type == ItemViewType.Reserve) {
+            btnNumber = 1;
+            firstBtn.setText(ItemViewType.Reserve.toString());
+            firstBtn.addActionListener(listener);
+            firstBtn.setActionCommand(Command.ITEM_RESERVED); // TODO
+            firstBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+            label.setText(this.item.getStatus().toString());
+            if (this.item.getStatus() == ItemStatus.Available) {
+                label.setForeground(new Color(24, 166, 22));
+                firstBtn.setEnabled(true);
+            } else {
+                label.setForeground(Color.RED);
+                firstBtn.setEnabled(false);
+            }
+            label.setAlignmentX(Component.CENTER_ALIGNMENT);
+            container.add(label);
+            container.add(Box.createRigidArea(new Dimension(5, 5)));
+        } else if (this.type == ItemViewType.Checkout) {
+            btnNumber = 0;
+        }
+        if (btnNumber > 0) {
+            container.add(firstBtn);
+            if (btnNumber == 2) {
+                container.add(Box.createRigidArea(new Dimension(5, 5)));
+                container.add(secondBtn);
+            }
+        }
     }
 
     public BaseItem getItem() {
