7d544235f22d91b2df99c218c93d78604cb2df60
srichs <sr@srich.us>
2020-12-11 14:41:54 -0500

    Added ItemStatus filter

--- src/main/java/edu/umgc/librarymanager/gui/panels/AdvancedSearchPanel.java
+++ src/main/java/edu/umgc/librarymanager/gui/panels/AdvancedSearchPanel.java
@@ -8,10 +8,12 @@ package edu.umgc.librarymanager.gui.panels;
 
 import edu.umgc.librarymanager.data.access.AdvSearchPart;
 import edu.umgc.librarymanager.data.access.LogicalType;
+import edu.umgc.librarymanager.data.model.item.ItemStatus;
 import edu.umgc.librarymanager.gui.Command;
 import edu.umgc.librarymanager.gui.DialogUtil;
 import edu.umgc.librarymanager.gui.GUIController;
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -19,9 +21,9 @@ import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -38,6 +40,7 @@ public class AdvancedSearchPanel extends JPanel {
     private LogicalFieldPanel lfPanelTwo;
     private LogicalFieldPanel lfPanelThree;
     private LogicalFieldPanel lfPanelFour;
+    private JComboBox<String> statusBox;
 
     /**
      * The constructor for the class.
@@ -50,6 +53,9 @@ public class AdvancedSearchPanel extends JPanel {
         this.lfPanelTwo = new LogicalFieldPanel();
         this.lfPanelThree = new LogicalFieldPanel();
         this.lfPanelFour = new LogicalFieldPanel();
+        String[] arr = {"", ItemStatus.Available.toString(), ItemStatus.CheckedOut.toString(),
+                ItemStatus.InTransit.toString(), ItemStatus.OnHold.toString()};
+        this.statusBox = new JComboBox<String>(arr);
         createPanel(control);
     }
 
@@ -65,16 +71,24 @@ public class AdvancedSearchPanel extends JPanel {
         titlePanel.add(titleLabel);
 
         JPanel searchPanel = new JPanel();
-        searchPanel.setLayout(new FlowLayout());
+        searchPanel.setLayout(new BoxLayout(searchPanel, BoxLayout.Y_AXIS));
         searchPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
         JPanel fieldHolder = new JPanel();
         fieldHolder.setPreferredSize(new Dimension(900, 200));
         fieldHolder.setLayout(new BoxLayout(fieldHolder, BoxLayout.Y_AXIS));
+        fieldHolder.setAlignmentX(Component.CENTER_ALIGNMENT);
         fieldHolder.add(this.lfPanelOne);
         fieldHolder.add(this.lfPanelTwo);
         fieldHolder.add(this.lfPanelThree);
         fieldHolder.add(this.lfPanelFour);
+        JPanel statusPanel = new JPanel();
+        statusPanel.setLayout(new FlowLayout());
+        statusPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        statusPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        statusPanel.add(new JLabel("Filter by Status "));
+        statusPanel.add(this.statusBox);
         searchPanel.add(fieldHolder);
+        searchPanel.add(statusPanel);
 
         JPanel footerPanel = new JPanel(new BorderLayout());
         JPanel backPanel = new JPanel(new FlowLayout());
@@ -120,22 +134,33 @@ public class AdvancedSearchPanel extends JPanel {
         return this.lfPanelFour;
     }
 
+    public JComboBox<String> getStatusBox() {
+        return this.statusBox;
+    }
+
+    /**
+     * Builds a List to be used in the building of the advanced search query.
+     * @return A List of AdvSearchPart objects.
+     */
     public List<AdvSearchPart> buildAdvanceSearch() {
         if (this.lfPanelOne.getSearchField().getText().equals("")) {
             DialogUtil.warningMessage("Please enter select a field and enter the search terms.", "Unable to Complete Search");
             return null;
         }
         List<AdvSearchPart> list = new ArrayList<AdvSearchPart>();
-        AdvSearchPart advSearch = new AdvSearchPart();
-        advSearch.setField(this.lfPanelOne.getFieldBoxValue());
-        advSearch.setSearchTerm(this.lfPanelOne.getSearchField().getText());
-        list.add(advSearch);
+        addSearchPart(this.lfPanelOne, list);
         addSearchPart(this.lfPanelTwo, list);
         addSearchPart(this.lfPanelThree, list);
         addSearchPart(this.lfPanelFour, list);
+        System.out.println(AdvSearchPart.listToString(list));
         return list;
     }
 
+    /**
+     * Used to add an AdvSearchPart object to a list that will be used to build the advanced search query.
+     * @param panel The panel to get the information for the AdvSearchPart object.
+     * @param list The List to add the AdvSearchPart object to.
+     */
     public void addSearchPart(LogicalFieldPanel panel, List<AdvSearchPart> list) {
         AdvSearchPart advSearch = checkForSearchPart(panel);
         if (advSearch != null) {
@@ -143,6 +168,11 @@ public class AdvancedSearchPanel extends JPanel {
         }
     }
 
+    /**
+     * Checks a LogicalFieldPanel to see if there is data that should be added for the advanced search query.
+     * @param panel The panel to check for information.
+     * @return An AdvSearchPart object of the panel's data.
+     */
     public AdvSearchPart checkForSearchPart(LogicalFieldPanel panel) {
         if (!panel.getSearchField().getText().equals("")) {
             AdvSearchPart advSearch = new AdvSearchPart();
@@ -154,11 +184,15 @@ public class AdvancedSearchPanel extends JPanel {
         return null;
     }
 
+    /**
+     * Used to reset the AdvancedSearchPanel to default values.
+     */
     public void reset() {
         this.lfPanelOne.reset();
         this.lfPanelTwo.reset();
         this.lfPanelThree.reset();
         this.lfPanelFour.reset();
+        this.statusBox.setSelectedIndex(0);
     }
 
 }
